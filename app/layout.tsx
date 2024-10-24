8525ac03a798e68d07da18a8af23bd02dbb28192
unknown <rickyit@hotmail.com>
2024-10-25 01:38:23 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/app/ui/global.css";
+import { outfit } from "@/app/ui/fonts";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${outfit.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Outfit } from "next/font/google";
+export const outfit = Outfit({ subsets: ["latin"] });
+import { Lusitana } from "next/font/google";
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
